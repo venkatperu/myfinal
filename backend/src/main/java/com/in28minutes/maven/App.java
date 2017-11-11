e5e1bca83602d13abe3baf073fb13156796bcbc7
EC2 Default User <ec2-user@ip-172-31-38-199.ec2.internal>
2017-11-11 08:57:14 +0000

    first code

---
+++ backend/src/main/java/com/in28minutes/maven/App.java
@@ -0,0 +1,17 @@
+package com.in28minutes.maven;
+
+/**
+ * Hello World test!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Test Test!" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
